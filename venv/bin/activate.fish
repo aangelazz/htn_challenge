344650b35f8ead6b099fd60c70069b7a71f49782
ANGELA ZHUANG <aangelazz.44@gmail.com>
2025-02-05 22:08:46 -0500

    updates

--- venv/bin/activate.fish
+++ venv/bin/activate.fish
@@ -29,7 +29,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/Users/angelazhuang/Documents/VS CODE/htn/venv"
+set -gx VIRTUAL_ENV "/Users/angelazhuang/Documents/vs/htn/venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
